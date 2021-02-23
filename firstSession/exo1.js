e6737e562629df65d5c0c67926c79b0d4a1b4786
Celine Lieutaud <celine.lieutaud@ynov.com>
2021-02-23 15:16:08 +0100

    Create folder + exo1.js
    Création du dossier et du repository ainsi que du premier exercice.

---
+++ firstSession/exo1.js
@@ -0,0 +1,14 @@
+// String Calculator
+
+/*
+    This classic kata guides you stp by step through the implementation of a calculator
+    that receives a Sring as input. It is a good exercise on refactoring and incremental
+    implementation. It's also a good candidate for practising TDD.
+*/
+
+
+/* First step
+    Create script add that takes a String and returns a String and use console.log()
+    to show the result.
+*/
+
