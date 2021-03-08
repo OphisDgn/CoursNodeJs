0184cf8f318dced3225ca8b9467eee62c0885f39
Celine Lieutaud <celine.lieutaud@ynov.com>
2021-03-08 13:00:13 +0100

    test git commit/push sur visual

--- listexo.js
+++ listexo.js
@@ -1,6 +1,4 @@
-// String Calculator
-
-/*
+/* String Calculator
     This classic kata guides you stp by step through the implementation of a calculator
     that receives a Sring as input. It is a good exercise on refactoring and incremental
     implementation. It's also a good candidate for practising TDD.
